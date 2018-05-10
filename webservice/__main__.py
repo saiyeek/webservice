0783627a8e34474dc61651920319de411c8f8449
Nitesh Pandey <Nitesh.Pandey@netapp.com>
2018-05-10 11:16:31 -0400

    Remove add get

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -36,7 +36,6 @@ async def main(request):
 if __name__ == "__main__":
     app = web.Application()
     app.router.add_post("/", main)
-    app.route.add_get("/", main)
     port = os.environ.get("PORT")
     if port is not None:
         port = int(port)
